a8b04bb402415631f913b92b8c032a83b065e09c
FaroukAjimi <benajimi@gmail.com>
2023-09-20 13:42:37 +0100

    Game4

--- src/views/Game.js
+++ src/views/Game.js
@@ -1,14 +1,10 @@
 import React from 'react';
 function Game() {
   return (
-    <div id='window'>
-    <div id='outer'>
       <iframe
         title="Unity Game"
         src="/Game/index.html" // Replace with the correct path to your Unity WebGL build
       ></iframe>
-      </div>
-    </div>
   );
 }
 
