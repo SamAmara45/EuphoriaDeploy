e1b8df459adc748973321e2ff33d152ffcd3e7df
FaroukAjimi <benajimi@gmail.com>
2023-09-20 13:45:50 +0100

    Game4

--- src/views/Game.js
+++ src/views/Game.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Game.css'
 function Game() {
   return (
+    <div id='window'>
       <iframe
         title="Unity Game"
         src="/Game/index.html" // Replace with the correct path to your Unity WebGL build
+        width="920"
+        height="600"
+        frameBorder="1"
       ></iframe>
+      <span className='name'>
+        Euphoria
+      </span>
+      </div>
   );
 }
 
